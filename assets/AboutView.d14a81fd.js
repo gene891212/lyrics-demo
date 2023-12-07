e1b8cfcbf270175048eb4c4d853a0636e2a49773
gene <gene891212@gmail.com>
2023-12-08 01:31:29 +0800

    deploy

---
+++ assets/AboutView.d14a81fd.js
@@ -0,0 +1,1 @@
+import{o as e,c as t,a as o}from"./index.e510ff93.js";const s={class:"about"},a=o("h1",null,"This is an about page",-1),c=[a],u={__name:"AboutView",setup(_){return(n,r)=>(e(),t("div",s,c))}};export{u as default};
